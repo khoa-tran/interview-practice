8f7d7c182f6128a67c81b8a53b4f8282ef21a219
Khoa Tran <akhoatr@gmail.com>
2018-03-13 15:00:31 -0700

    use assertj instead

--- src/test/java/com/akta/list/MatrixTraverseTest.java
+++ src/test/java/com/akta/list/MatrixTraverseTest.java
@@ -5,7 +5,7 @@ import static com.akta.list.MatrixTraverse.zigzag;
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static java.util.Collections.singletonList;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,19 +15,19 @@ public class MatrixTraverseTest {
 
   @Test
   public void zigzag_withNull() {
-    assertEquals(emptyList(), zigzag(null));
+    assertThat(zigzag(null)).isEmpty();
   }
 
   @Test
   public void zigzag_withEmpty() {
-    assertEquals(emptyList(), zigzag(emptyList()));
+    assertThat(zigzag(emptyList())).isEmpty();
   }
 
   @Test
   public void zigzag_withOnlyOneRow() {
     List<List<Integer>> matrix = new ArrayList<>();
     matrix.add(asList(1, 2, 3, 4));
-    assertEquals(asList(1, 2, 3, 4), zigzag(matrix));
+    assertThat(zigzag(matrix)).containsExactly(1, 2, 3, 4);
   }
 
   @Test
@@ -37,7 +37,7 @@ public class MatrixTraverseTest {
     matrix.add(singletonList(2));
     matrix.add(singletonList(3));
     matrix.add(singletonList(4));
-    assertEquals(asList(1, 2, 3, 4), zigzag(matrix));
+    assertThat(zigzag(matrix)).containsExactly(1, 2, 3, 4);
   }
 
   @Test
@@ -46,7 +46,7 @@ public class MatrixTraverseTest {
     matrix.add(asList(1, 2, 3, 4));
     matrix.add(asList(5, 6, 7, 8));
     matrix.add(asList(9, 10, 11, 12));
-    assertEquals(asList(1, 2, 3, 4, 8, 7, 6, 5, 9, 10, 11, 12), zigzag(matrix));
+    assertThat(zigzag(matrix)).containsExactly(1, 2, 3, 4, 8, 7, 6, 5, 9, 10, 11, 12);
   }
 
   @Test
@@ -56,7 +56,7 @@ public class MatrixTraverseTest {
     matrix.add(asList(3, 4, 5, 6, 7));
     matrix.add(asList(8, 9, 10));
     matrix.add(asList(11, 12, 13, 14));
-    assertEquals(asList(1, 2, 7, 6, 5, 4, 3, 8, 9, 10, 14, 13, 12, 11), zigzag(matrix));
+    assertThat(zigzag(matrix)).containsExactly(1, 2, 7, 6, 5, 4, 3, 8, 9, 10, 14, 13, 12, 11);
   }
 
   @Test
@@ -65,14 +65,14 @@ public class MatrixTraverseTest {
     matrix.add(null);
     matrix.add(emptyList());
     matrix.add(singletonList(1));
-    assertEquals(singletonList(1), zigzag(matrix));
+    assertThat(zigzag(matrix)).containsExactly(1);
 
     matrix.add(asList(2, 3));
-    assertEquals(asList(1, 3, 2), zigzag(matrix));
+    assertThat(zigzag(matrix)).containsExactly(1, 3, 2);
 
     matrix.add(emptyList());
     matrix.add(asList(4, 5, 6));
-    assertEquals(asList(1, 3, 2, 4, 5, 6), zigzag(matrix));
+    assertThat(zigzag(matrix)).containsExactly(1, 3, 2, 4, 5, 6);
 
     matrix.add(null);
     matrix.add(null);
@@ -80,24 +80,24 @@ public class MatrixTraverseTest {
     matrix.add(emptyList());
     matrix.add(null);
     matrix.add(asList(7, 8));
-    assertEquals(asList(1, 3, 2, 4, 5, 6, 8, 7), zigzag(matrix));
+    assertThat(zigzag(matrix)).containsExactly(1, 3, 2, 4, 5, 6, 8, 7);
   }
 
   @Test
   public void spiral_withNull() {
-    assertEquals(emptyList(), spiral(null));
+    assertThat(spiral(null)).isEmpty();
   }
 
   @Test
   public void spiral_withEmpty() {
-    assertEquals(emptyList(), spiral(emptyList()));
+    assertThat(spiral(null)).isEmpty();
   }
 
   @Test
   public void spiral_withOnlyOneRow() {
     List<List<Integer>> matrix = new ArrayList<>();
     matrix.add(asList(1, 2, 3, 4));
-    assertEquals(asList(1, 2, 3, 4), spiral(matrix));
+    assertThat(spiral(matrix)).containsExactly(1, 2, 3, 4);
   }
 
   @Test
@@ -107,7 +107,7 @@ public class MatrixTraverseTest {
     matrix.add(singletonList(2));
     matrix.add(singletonList(3));
     matrix.add(singletonList(4));
-    assertEquals(asList(1, 2, 3, 4), spiral(matrix));
+    assertThat(spiral(matrix)).containsExactly(1, 2, 3, 4);
   }
 
   @Test
@@ -116,9 +116,7 @@ public class MatrixTraverseTest {
     matrix.add(asList(1, 2, 3, 4, 5));
     matrix.add(asList(6, 7, 8, 9, 10));
     matrix.add(asList(11, 12, 13, 14, 15));
-    assertEquals(
-        asList(1, 2, 3, 4, 5, 10, 15, 14, 13, 12, 11, 6, 7, 8, 9),
-        spiral(matrix));
+    assertThat(spiral(matrix)).containsExactly(1, 2, 3, 4, 5, 10, 15, 14, 13, 12, 11, 6, 7, 8, 9);
   }
 
   @Test
@@ -128,9 +126,8 @@ public class MatrixTraverseTest {
     matrix.add(asList(6, 7, 8, 9, 10));
     matrix.add(asList(11, 12, 13, 14, 15));
     matrix.add(asList(16, 17, 18, 19, 20));
-    assertEquals(
-        asList(1, 2, 3, 4, 5, 10, 15, 20, 19, 18, 17, 16, 11, 6, 7, 8, 9, 14, 13, 12),
-        spiral(matrix));
+    assertThat(spiral(matrix))
+        .containsExactly(1, 2, 3, 4, 5, 10, 15, 20, 19, 18, 17, 16, 11, 6, 7, 8, 9, 14, 13, 12);
   }
 
   @Test
@@ -138,7 +135,7 @@ public class MatrixTraverseTest {
     List<List<Integer>> matrix = new ArrayList<>();
     matrix.add(asList(1, 2));
     matrix.add(asList(3, 4));
-    assertEquals(asList(1, 2, 4, 3), spiral(matrix));
+    assertThat(spiral(matrix)).containsExactly(1, 2, 4, 3);
   }
 
   @Test
@@ -148,9 +145,8 @@ public class MatrixTraverseTest {
     matrix.add(asList(5, 6, 7, 8));
     matrix.add(asList(9, 10, 11, 12));
     matrix.add(asList(13, 14, 15, 16));
-    assertEquals(
-        asList(1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10),
-        spiral(matrix));
+    assertThat(spiral(matrix))
+        .containsExactly(1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10);
   }
 
 }
